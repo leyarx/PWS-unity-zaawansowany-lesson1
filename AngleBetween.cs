1be29772de201ca78ce9bca6c76a6bde82181def
leyarx <leyarx@gmail.com>
2021-02-09 20:19:47 +0200

    task3

--- AngleBetween.cs
+++ AngleBetween.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class AngleBetween : MonoBehaviour
 {
     public GameObject enemy;
+    float enemyBoundsRadius = 1f;
 
     Vector3 playerDirection = Vector3.forward;
     Vector3 playerRight;
+    float playerBoundsRadius = 1f;
 
     private void Start()
     {
@@ -26,15 +28,34 @@ public class AngleBetween : MonoBehaviour
     void Update()
     {
         Vector3 vec = enemy.transform.position - this.transform.position;
-        Debug.Log(Mathf.Acos(Vector3.Dot(playerDirection, vec.normalized)) * Mathf.Rad2Deg);
 
-        if(Vector3.Dot(playerRight, vec.normalized) < 0)
+        if (playerBoundsRadius + enemyBoundsRadius > vec.magnitude)
         {
-            Debug.Log("right");
+            Debug.Log("Collision!");
         }
         else
         {
-            Debug.Log("left");
+            float angle = Mathf.Acos(Vector3.Dot(playerDirection, vec.normalized)) * Mathf.Rad2Deg;
+            Debug.Log("Angle between player and enemy: " + angle);
+
+            if(angle < 90)
+            {
+                Debug.Log("front");
+            }
+            else
+            {
+                Debug.Log("back");
+            }
+
+
+            if (Vector3.Dot(playerRight, vec.normalized) < 0)
+            {
+                Debug.Log("right");
+            }
+            else
+            {
+                Debug.Log("left");
+            }
         }
     }
 }
