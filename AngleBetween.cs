6856013e5c4e3cce80146b423a613694725a3071
leyarx <leyarx@gmail.com>
2021-02-09 19:33:13 +0200

    task1

---
+++ AngleBetween.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleBetween : MonoBehaviour
+{
+    public GameObject enemy;
+
+    Vector3 playerDirection = Vector3.forward;
+
+    void Update()
+    {
+        if(enemy)
+        {
+            Vector3 vec = enemy.transform.position - this.transform.position;
+            Debug.Log(Mathf.Acos(Vector3.Dot(playerDirection, vec.normalized)) * Mathf.Rad2Deg);
+        }
+    }
+}
