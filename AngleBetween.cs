b2db3770da65dbceb85c20eddade3e9ab19970e0
leyarx <leyarx@gmail.com>
2021-02-09 20:00:15 +0200

    task2

--- AngleBetween.cs
+++ AngleBetween.cs
@@ -7,13 +7,34 @@ public class AngleBetween : MonoBehaviour
     public GameObject enemy;
 
     Vector3 playerDirection = Vector3.forward;
+    Vector3 playerRight;
+
+    private void Start()
+    {
+        playerRight = Vector3.Cross(playerDirection, Vector3.up);
+        setImportantVariable();
+    }
+    
+    private void setImportantVariable() 
+    { 
+        if (!enemy)
+        {
+            this.enabled = false;
+        }
+    }
 
     void Update()
     {
-        if(enemy)
+        Vector3 vec = enemy.transform.position - this.transform.position;
+        Debug.Log(Mathf.Acos(Vector3.Dot(playerDirection, vec.normalized)) * Mathf.Rad2Deg);
+
+        if(Vector3.Dot(playerRight, vec.normalized) < 0)
         {
-            Vector3 vec = enemy.transform.position - this.transform.position;
-            Debug.Log(Mathf.Acos(Vector3.Dot(playerDirection, vec.normalized)) * Mathf.Rad2Deg);
+            Debug.Log("right");
+        }
+        else
+        {
+            Debug.Log("left");
         }
     }
 }
